54464c29b395468e9ce576d220d912a41e2e24bd
Vojtěch Šobáň <vojtech.soban@gmail.com>
2019-05-20 15:39:40 +0200

    Removed eslint, added sumOddNumbers demo

--- src/sum.js
+++ src/sum.js
@@ -8,3 +8,8 @@
 export function solution(X, Y) {
   return X + Y;
 }
+
+
+export function sumOddNumbers(...params) {
+  return params.filter((number => number % 2 === 1)).reduce((acc, value) => acc + value, 0)
+}
--- src/sum.spec.js
+++ src/sum.spec.js
@@ -1,16 +1,31 @@
 import { expect } from "chai";
-import { solution } from "./sum";
+import { solution, sumOddNumbers } from "./sum";
 
-describe("sum", function() {
-  [
+describe("sum", () => {
+	describe("sum parameterized demo", () => {
+    [
     {
       X: 1,
       Y: 2,
       expected: 3
     }
-  ].forEach(context => {
-    it(`X: ${context.X} Y: ${context.Y} `, function() {
-      expect(solution(context.X, context.Y)).to.eql(context.expected);
+    ].forEach(context => {
+      it(`X: ${context.X} Y: ${context.Y} `, function() {
+        expect(solution(context.X, context.Y)).to.be.equal(context.expected);
+      });
     });
   });
+  describe("sum manual demo", () => {
+    it("6 + 66 = 72", () => {
+      expect(solution(6, 66)).to.be.equal(72)
+    })
+  })
+  describe("sum odd numbers demo", () => {
+    it("1 + 2 + 3 + 4 = 4", () => {
+      expect(sumOddNumbers(1, 2, 3, 4)).to.be.equal(4)
+    })
+    it("2 + 4 + 6 = 0", () => {
+      expect(sumOddNumbers(2, 4, 6)).to.be.equal(0)
+    })
+  })
 });
